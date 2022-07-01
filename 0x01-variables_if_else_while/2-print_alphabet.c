33fb509c11f4200080a5409de7a668c046b3f462
olorunwapelumi <olorunwapelumi@yahoo.com>
2022-07-01 05:46:32 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Description: 'task 2'
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
+putchar('\n');
+return (0);
+}
