e24bdc1230052be0170510684451c334dabd4d8b
olorunwapelumi <olorunwapelumi@yahoo.com>
2022-06-30 23:04:00 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description 'task 8'
+ *
+ * Return: 0 if successful, a nonzero otherwise*
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
